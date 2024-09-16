0e3919e3c88180cd57ad32a6840b4a3640a10975
pablitohaddad <pablohaddad73@gmail.com>
2024-09-16 18:41:43 -0400

    Adicionando JS ao projeto e configurações adicionais

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d8a62704f2be33baa2489edab258a331c192fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52af795278aac5383b9cd24d44bd2da97721c99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
